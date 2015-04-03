b4e7993c0876b6812db69de09f715b182bfd43ac
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-04-03 16:48:02 -0700

    got x and o inside boxes

--- js/app.js
+++ js/app.js
@@ -1,31 +1,4 @@
-var player = 'player1_logo.png';
-
-// "grabs" the class 'box' from the css file
-
-var boxes = document.getElementsByClassName('box');
-console.log(boxes);
-
-// iterates through every box on the game board
-
-for (var i = 0; i < boxes.length; i++) {
-	boxes[i].onclick = function() {
-		this.style.backgroundImage = "url(images/" + player +")";
-		switchPlayer();
-	}
-}
-
-// switches the current click from player 1's click to player 2's click
-
-function switchPlayer() {
-	if (player === "player1_logo.png") {
-		player = "blue";
-	}else {
-		player = "player1_logo.png";
-	}
-}
-
-// prevents a selected box from being overwritten
-
-function click() {
-	console.log("hello");
-}
+// IIFE 
+(function(){
+	angular.module('ticTacToeApp', []);
+})();
--- js/ticTacToeController.js
+++ js/ticTacToeController.js
@@ -4,8 +4,63 @@
 		.module('ticTacToeApp')
 		.controller('ticTacToeController', ticTacToeController);
 
+
+		// the controller itself (allows acces between the model, or data, and the view, or user interface)
 		function ticTacToeController(){
-			// Capture variable that references the Controller
+			// capture variable that references the Controller
 			var self = this;
-		}
-})();
+
+				// determines who is currently playing
+			var turn = 1;
+
+
+			// prevents the clicked object from being overwritten
+			self.click = function($index) {
+					console.log($index);
+
+					if (self.board[$index].space) {
+						return false;
+					}
+					if (turn === 1){
+						self.board[$index].space = "X";
+						turn++;
+					}else {
+						self.board[$index].space = "O";
+						turn--;
+					}
+			};
+		
+
+
+// creates an object that makes 9 cells on the game board (by replicating it 9 times)
+
+	self.board = [
+		{space: null},
+		{space: null},
+		{space: null},
+		{space: null},
+		{space: null},
+		{space: null},
+		{space: null},
+		{space: null},
+		{space: null},
+		];
+
+// places an image inside of the box that is clicked (selected)
+
+function playerMove() {
+	 self.click.cells = alert("you win");
+}
+
+
+
+
+
+
+
+}
+})(); // ends IIFE
+
+
+
+	

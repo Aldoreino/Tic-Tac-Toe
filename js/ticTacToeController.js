6b45a012757b0126a8cccdd92dccdb27c65c6306
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-04-05 16:24:37 -0700

    revised game logic for O wins

--- js/ticTacToeController.js
+++ js/ticTacToeController.js
@@ -19,35 +19,62 @@
 function checkForWinnerX() {
 		// checks for wins in a horizontal direction
 		if (self.board[0].space === "X" && self.board[1].space === "X" && self.board[2].space === "X"){
-			alert("you win");
+			alert("X wins");
 		}
 		if (self.board[3].space === "X" && self.board[4].space === "X" && self.board[5].space === "X"){
-			alert("you win");
+			alert("X wins");
 
 		}
 		if (self.board[6].space === "X" && self.board[7].space === "X" && self.board[8].space === "X"){
-			alert("you win");
+			alert("X wins");
 		}
 		// checks for wins in a vertical dierction
 		if (self.board[0].space === "X" && self.board[3].space === "X" && self.board[6].space === "X"){
-			alert("you win");
+			alert("X wins");
 		}
 		if (self.board[1].space === "X" && self.board[4].space === "X" && self.board[7].space === "X"){
-			alert("you win");
+			alert("X wins");
 		}
 		if (self.board[2].space === "X" && self.board[5].space === "X" && self.board[8].space === "X"){
-			alert("you win");
+			alert("X wins");
 		}
 		// checks for wins in a diagonal direction
 		if (self.board[0].space === "X" && self.board[4].space === "X" && self.board[8].space === "X"){
-			alert("you win");
+			alert("X wins");
 		}
 		if (self.board[2].space === "X" && self.board[4].space === "X" && self.board[6].space === "X"){
-			alert("you win");
+			alert("X wins");
 		}
 }
-
-
+		// checks to see if O has won in a horizontal direction
+		function checkForWinnerO() {
+			if (self.board[0].space === "O" && self.board[1].space ==="O" && self.board[2].space === "O"){
+				alert("O wins");
+			}
+			if (self.board[3].space === "O" && self.board[4].space === "O" && self.board[5].space === "O"){
+				alert("O wins");
+			}
+			if (self.board[6].space === "O" && self.board[7].space === "O" && self.board[8].space === "O"){
+				alert("O wins");
+			}
+			// checks to see if O has won in a vertical direction			
+			if (self.board[0].space === "O" && self.board[3].space === "O" && self.board[6].space === "O"){
+				alert("O wins");
+			}			
+			if (self.board[1].space === "O" && self.board[4].space === "O" && self.board[7].space === "O"){
+				alert("O wins");
+			}			
+			if (self.board[2].space === "O" && self.board[5].space === "O" && self.board[8].space === "O"){
+				alert("O wins");
+			}
+			// checks to see if O has won in a diagonal direction			
+			if (self.board[0].space === "O" && self.board[4].space === "O" && self.board[8].space === "O"){
+				alert("O wins");
+			}			
+			if (self.board[2].space === "O" && self.board[4].space === "O" && self.board[6].space === "O"){
+				alert("O wins");
+			}
+		}
 
 
 
@@ -68,12 +95,13 @@ function checkForWinnerX() {
 						turn++;
 					//checks for winner after move has been made
 						checkForWinnerX();
+					
 					}else {
 						self.board[$index].space = "O";
 
 						turn--;
 					// check for winner after move has been made
-						checkForWinnerX();
+						checkForWinnerO();
 					}
 			};
 

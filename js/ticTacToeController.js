a5853fddee2da7a2091b098f943e3eb04acd3a9d
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-04-05 18:20:35 -0700

    started Draw logic

--- js/ticTacToeController.js
+++ js/ticTacToeController.js
@@ -12,6 +12,10 @@
 
 				// determines who is currently playing
 			var turn = 1;
+			// subject to change!!!!
+			var snd = new Audio("audio.mp3/alert_sound.mp3");
+
+
 
 			
 
@@ -19,71 +23,91 @@
 function checkForWinnerX() {
 		// checks for wins in a horizontal direction
 		if (self.board[0].space === "X" && self.board[1].space === "X" && self.board[2].space === "X"){
-			alert("X wins");
+			snd.play();
+			alert("X wins"); 
 		}
 		if (self.board[3].space === "X" && self.board[4].space === "X" && self.board[5].space === "X"){
+			snd.play();
 			alert("X wins");
 
 		}
 		if (self.board[6].space === "X" && self.board[7].space === "X" && self.board[8].space === "X"){
+			snd.play();
 			alert("X wins");
 		}
 		// checks for wins in a vertical dierction
 		if (self.board[0].space === "X" && self.board[3].space === "X" && self.board[6].space === "X"){
+			snd.play();
 			alert("X wins");
 		}
 		if (self.board[1].space === "X" && self.board[4].space === "X" && self.board[7].space === "X"){
+			snd.play();
 			alert("X wins");
 		}
 		if (self.board[2].space === "X" && self.board[5].space === "X" && self.board[8].space === "X"){
+			snd.play();
 			alert("X wins");
 		}
 		// checks for wins in a diagonal direction
 		if (self.board[0].space === "X" && self.board[4].space === "X" && self.board[8].space === "X"){
+			snd.play();
 			alert("X wins");
 		}
 		if (self.board[2].space === "X" && self.board[4].space === "X" && self.board[6].space === "X"){
+			snd.play();
 			alert("X wins");
 		}
-}
+	}// end of function "checkForWinnerX"
+
 		// checks to see if O has won in a horizontal direction
 		function checkForWinnerO() {
 			if (self.board[0].space === "O" && self.board[1].space ==="O" && self.board[2].space === "O"){
+				snd.play();
 				alert("O wins");
 			}
 			if (self.board[3].space === "O" && self.board[4].space === "O" && self.board[5].space === "O"){
 				alert("O wins");
 			}
 			if (self.board[6].space === "O" && self.board[7].space === "O" && self.board[8].space === "O"){
+				snd.play();
 				alert("O wins");
 			}
 			// checks to see if O has won in a vertical direction			
 			if (self.board[0].space === "O" && self.board[3].space === "O" && self.board[6].space === "O"){
+				snd.play();
 				alert("O wins");
 			}			
 			if (self.board[1].space === "O" && self.board[4].space === "O" && self.board[7].space === "O"){
+				snd.play();
 				alert("O wins");
 			}			
 			if (self.board[2].space === "O" && self.board[5].space === "O" && self.board[8].space === "O"){
+				snd.play();
 				alert("O wins");
 			}
 			// checks to see if O has won in a diagonal direction			
 			if (self.board[0].space === "O" && self.board[4].space === "O" && self.board[8].space === "O"){
+				snd.play();
 				alert("O wins");
 			}			
 			if (self.board[2].space === "O" && self.board[4].space === "O" && self.board[6].space === "O"){
+				snd.play();
 				alert("O wins");
+			}
+		} // end of function "checkForWinnerO"
+
+		function Draw() {
+			if ("checkForWinnerO" && "checkForWinnerX" === false){
+				alert("the game is a draw!")
 			}
 		}
 
 
 
 
-
-
 			// prevents the clicked object from being overwritten
 			self.click = function($index) {
-					console.log($index);
+					Draw();
 
 					if (self.board[$index].space) {
 						return false;
